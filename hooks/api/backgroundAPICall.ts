b74ec22e75d533e22f86c89d3e975428ab8cedb0
Sukumar-1706 <sukumarmee@gmail.com>
2025-10-30 18:22:48 +0530

    api updated

--- hooks/api/backgroundAPICall.ts
+++ hooks/api/backgroundAPICall.ts
@@ -1,10 +1,10 @@
-import apiClient from '../logger/apiClient';
 
+import axios from "axios";
 export const  makeApiCall= async()=> {
     // console.log(formData);
     try {
         let apiSharedfileUrl = 'https://67386c2e4eb22e24fca7e0a5.mockapi.io/cloudbook/api/reminder'; // Fallback URL
-        const response = await apiClient.get(apiSharedfileUrl);
+        const response = await axios.get(apiSharedfileUrl);
     //   console.log(response.data);
     return response.data
     } catch (error) {
